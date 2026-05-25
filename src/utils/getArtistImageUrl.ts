c153f393be9276892ffede0174eaa6e31bbc4957
Frank Paulino <fvpg@hotmail.com>
2026-05-25 08:38:48 -0400

    Update

--- src/utils/getArtistImageUrl.ts
+++ src/utils/getArtistImageUrl.ts
@@ -1,4 +1,4 @@
 export function getArtistImageUrl(artistId: string) {
   const base = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/artists-images`;
-  return `${base}/${artistId}.jpg`; // or .webp if that's your new standard
+  return `${base}/${artistId}.webp`; // or .webp if that's your new standard
 }
